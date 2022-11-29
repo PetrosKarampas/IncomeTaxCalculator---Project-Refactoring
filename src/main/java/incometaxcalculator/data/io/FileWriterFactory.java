675bc8302b2ea8b289a9a7bef2ef8969c90f7107
PetrosKarampas <petros6_97@yahoo.gr>
2022-11-29 09:29:20 +0200

    refactor to java 11

--- src/main/java/incometaxcalculator/data/io/FileWriterFactory.java
+++ src/main/java/incometaxcalculator/data/io/FileWriterFactory.java
@@ -9,13 +9,19 @@ public class FileWriterFactory {
 
     public FileWriterInterface createFileWriter(@NotNull String format) throws WrongFileFormatException
     {
-        return switch (format) {
-            case "_INFO.txt" -> new TXTInfoWriter();
-            case "_INFO.xml" -> new XMLInfoWriter();
-            case "_LOG.txt"  -> new TXTLogWriter();
-            case "_LOG.xml"  -> new XMLLogWriter();
-            default          -> throw new WrongFileFormatException();
-        };
+        switch (format) {
+            case "_INFO.txt":
+                return new TXTInfoWriter();
+            case "_INFO.xml":
+                return new XMLInfoWriter();
+            case "_LOG.txt":
+                return new TXTLogWriter();
+            case "_LOG.xml":
+                return new XMLLogWriter();
+            default:
+                throw new WrongFileFormatException();
+        }
+
     }
 
 }
--- src/main/java/incometaxcalculator/data/management/TaxpayerFactory.java
+++ src/main/java/incometaxcalculator/data/management/TaxpayerFactory.java
@@ -7,17 +7,17 @@ public class TaxpayerFactory {
     public TaxpayerFactory() {}
 
     public Taxpayer createTaxpayer(String status, String fullName, int taxRegistrationNumber, float income) throws WrongTaxpayerStatusException{
-        return switch (status) {
-            case "Married Filing Jointly"
-                    -> new MarriedFilingJointlyTaxpayer(fullName, taxRegistrationNumber, income);
-            case "Married Filing Separately"
-                    -> new MarriedFilingSeparatelyTaxpayer(fullName, taxRegistrationNumber, income);
-            case "Single"
-                    -> new SingleTaxpayer(fullName, taxRegistrationNumber, income);
-            case "Head of Household"
-                    -> new HeadOfHouseholdTaxpayer(fullName, taxRegistrationNumber, income);
-            default
-                    -> throw new WrongTaxpayerStatusException();
-        };
+         switch (status) {
+            case "Married Filing Jointly":
+                    return new MarriedFilingJointlyTaxpayer(fullName, taxRegistrationNumber, income);
+            case "Married Filing Separately":
+                    return new MarriedFilingSeparatelyTaxpayer(fullName, taxRegistrationNumber, income);
+            case "Single":
+                    return new SingleTaxpayer(fullName, taxRegistrationNumber, income);
+            case "Head of Household":
+                    return new HeadOfHouseholdTaxpayer(fullName, taxRegistrationNumber, income);
+            default:
+                    throw new WrongTaxpayerStatusException();
+        }
     }
 }
